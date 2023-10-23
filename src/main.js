bb94f565f5ec8aaf2ecea0f389fbb786853399b2
Stanislav <stanislav.nechaev@gmail.com>
2023-10-24 02:00:49 +0300

    update and add router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import './assets/main.css'
 
-createApp(App).mount('#app')
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from "@/routers/router.js";
+
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
