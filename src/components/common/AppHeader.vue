5d4de342ffa97fdf54d03233113ba952a17bbeaf
Stanislav <stanislav.nechaev@gmail.com>
2023-10-24 17:06:00 +0300

    Create AppHeader.vue

---
+++ src/components/common/AppHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <header class="flex items-center gap-3 container mx-auto">
+    <app-logo/>
+    <ul class="flex flex-wrap gap-3 justify-center px-8 mx-auto">
+      <li>
+        <base-link :to="{name: 'home'}"/>
+        home
+      </li>
+      <li>
+        <base-link :to="{name: 'podiumBiPortal'}"/>
+        podiumBiPortal
+      </li>
+      <li>
+        <base-link :to="{name: 'walmartRetailData'}"/>
+        walmartRetailData
+      </li>
+      <li>
+        <base-link :to="{name: 'contact'}"/>
+        contact
+      </li>
+      <li>
+        <base-link :to="{name: 'blog'}"/>
+        blog
+      </li>
+    </ul>
+  </header>
+</template>
+
+<script setup>
+
+import AppLogo from "@/components/UI/AppLogo.vue";
+import BaseLink from "@/components/UI/BaseLink.vue";
+</script>
+
+<style scoped>
+
+</style>
