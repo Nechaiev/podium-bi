b0efb3fdc0eba15558b3cc726b6d4bd1919706e3
Stanislav <stanislav.nechaev@gmail.com>
2023-10-24 17:05:37 +0300

    Create AppLayout.vue

---
+++ src/layouts/AppLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <app-header/>
+
+    <main>
+      <slot/>
+    </main>
+
+    <footer>
+
+    </footer>
+  </div>
+</template>
+
+<script setup>
+import AppHeader from "@/components/common/AppHeader.vue";
+</script>
+
+<style scoped>
+
+</style>
