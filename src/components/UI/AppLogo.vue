baf75843f86c21ff94965ecab4f8ed5fdef09947
Stanislav <stanislav.nechaev@gmail.com>
2023-10-24 17:05:50 +0300

    Create AppLogo.vue

---
+++ src/components/UI/AppLogo.vue
@@ -0,0 +1,14 @@
+<template>
+  <router-link to="/" class="text-pink-600">
+    <IconLogo/>
+  </router-link>
+</template>
+
+<script setup>
+
+import IconLogo from "@/components/UI/icons/Logo.vue";
+</script>
+
+<style scoped>
+
+</style>
