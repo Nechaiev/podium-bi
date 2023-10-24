bda9bbde2ee961acb1023b90732529ecdfbef310
Stanislav <stanislav.nechaev@gmail.com>
2023-10-24 17:04:30 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryColor: 'var(--primaryColor)',
+        secondaryColor: 'var(--secondaryColor)',
+      },
+      fontSize: {
+        title_1: 'var(--title_1)',
+        title_2: 'var(--title_2)',
+        title_3: 'var(--title_3)',
+        title_4: 'var(--title_4)',
+        title_5: 'var(--title_5)',
+        title_6: 'var(--title_6)',
+      },
+      fontFamily: {
+        fontPrimary: 'var(--fontPrimary)',
+        fontSecondary: 'var(--fontSecondary)',
+      },
+    },
   },
   plugins: [],
 }
