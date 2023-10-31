9e5baeedf1374adef16f448e2e29d254d534bc47
Stanislav <stanislav.nechaev@gmail.com>
2023-10-31 15:27:05 +0200

    add middleware to router

--- src/routers/router.js
+++ src/routers/router.js
@@ -6,34 +6,46 @@ import PodiumBiPortal from "@/views/PodiumBiPortal.vue";
 import WalmartRetailData from "@/views/WalmartRetailData.vue";
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: Home
-        },
-        {
-            path: '/blog',
-            name: 'blog',
-            component: Blog
-        },
-        {
-            path: '/contact',
-            name: 'contact',
-            component: Contact
-        },
-        {
-            path: '/podiumBiPortal',
-            name: 'podiumBiPortal',
-            component: PodiumBiPortal
-        },
-        {
-            path: '/walmartRetailData',
-            name: 'walmartRetailData',
-            component: WalmartRetailData
-        }
-    ]
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/blog',
+      name: 'blog',
+      component: Blog
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: Contact
+    },
+    {
+      path: '/podiumBiPortal',
+      name: 'podiumBiPortal',
+      component: PodiumBiPortal
+    },
+    {
+      path: '/walmartRetailData',
+      name: 'walmartRetailData',
+      component: WalmartRetailData,
+      meta: {
+        private: true,
+      }
+    }
+  ]
+});
+// add middleware to router
+window.isAuth = true;
+
+router.beforeEach((to, from) => {
+  console.log(to, from)
+  if (to.meta.private) {
+    return window.isAuth
+  }
 })
 
 export default router
