c4fe2b540472ebeec028ee294c5720d40363d31a
Stanislav <stanislav.nechaev@gmail.com>
2023-11-01 20:04:59 +0200

    Update to Composition

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,23 +1,64 @@
-import { defineStore } from "pinia";
+import {defineStore} from "pinia";
+import {ref} from "vue";
 
-export const useAuthStore = defineStore("auth", {
-  state: () => ({
-    isAuthenticated: false,
-    user: null,
-  }),
-  actions: {
-    login(username, password) {
-      if (username === "admin" && password === "admin123") {
-        this.isAuthenticated = true;
-        this.user = { username };
-      } else {
-        console.log("Неправильні дані для входу");
-        //додати трай кетч
-      }
-    },
-    logout() {
-      this.isAuthenticated = false;
-      this.user = null;
-    },
-  },
+export const useAuthStore = defineStore("auth", () => {
+  const isAuthenticated = ref(false);
+  const user = ref(null);
+  const login = (username, password) => {
+    if (username && password) {
+      isAuthenticated.value = true;
+      user.value = { username };
+    } else {
+      window.alert("Login and password are required");
+    }
+  };
+  const logout = () => {
+    isAuthenticated.value = false;
+    user.value = null;
+  };
+  return {
+    isAuthenticated,
+    user,
+    login,
+    logout,
+  };
 });
+
+
+// export const useAuthStore = defineStore("auth", {
+//   state: () => ({
+//     isAuthenticated: false,
+//     user: null,
+//   }),
+//   actions: {
+//     login(username, password) {
+//       this.isAuthenticated = true;
+//       this.user = {username};
+//     },
+//     logout() {
+//       this.isAuthenticated = false;
+//       this.user = null;
+//     },
+//   },
+// });
+
+// export const useAuthStore = defineStore("auth", {
+//   state: () => ({
+//     isAuthenticated: false,
+//     user: null,
+//   }),
+//   actions: {
+//     login(username, password) {
+//       if (username === "admin" && password === "admin123") {
+//         this.isAuthenticated = true;
+//         this.user = { username };
+//       } else {
+//         console.log("Неправильні дані для входу");
+//       }
+//     },
+//     logout() {
+//       this.isAuthenticated = false;
+//       this.user = null;
+//     },
+//   },
+// });
