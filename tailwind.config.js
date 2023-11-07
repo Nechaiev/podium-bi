c5dfce54c0a8fb2a6b074a8995b73ea05c8f4984
Stanislav <stanislav.nechaev@gmail.com>
2023-11-07 12:42:10 +0200

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,22 +6,22 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        primaryColor: 'var(--primaryColor)',
-        secondaryColor: 'var(--secondaryColor)',
-      },
-      fontSize: {
-        title_1: 'var(--title_1)',
-        title_2: 'var(--title_2)',
-        title_3: 'var(--title_3)',
-        title_4: 'var(--title_4)',
-        title_5: 'var(--title_5)',
-        title_6: 'var(--title_6)',
-      },
-      fontFamily: {
-        fontPrimary: 'var(--fontPrimary)',
-        fontSecondary: 'var(--fontSecondary)',
-      },
+      // colors: {
+      //   primaryColor: 'var(--primaryColor)',
+      //   secondaryColor: 'var(--secondaryColor)',
+      // },
+      // fontSize: {
+      //   title_1: 'var(--title_1)',
+      //   title_2: 'var(--title_2)',
+      //   title_3: 'var(--title_3)',
+      //   title_4: 'var(--title_4)',
+      //   title_5: 'var(--title_5)',
+      //   title_6: 'var(--title_6)',
+      // },
+      // fontFamily: {
+      //   fontPrimary: 'var(--fontPrimary)',
+      //   fontSecondary: 'var(--fontSecondary)',
+      // },
     },
   },
   plugins: [],
