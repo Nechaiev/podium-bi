bd389d99cb2fafb833f52fa045a82ef2b4a2e1de
Stanislav <stanislav.nechaev@gmail.com>
2023-11-14 01:45:19 +0200

    Create BaseLoading.vue

---
+++ src/components/UI/BaseLoading.vue
@@ -0,0 +1,26 @@
+<template>
+  <div :class="loadingStyle">
+    Loading...
+  </div>
+</template>
+<script setup>
+import {computed} from 'vue';
+const props = defineProps({
+  text: String,
+  loadingStyle: {
+    type: String,
+    validator(value) {
+      return ['text', 'animation'].includes(value)
+    },
+    default: 'text',
+  }
+})
+
+const loadingStyle = computed(() => {
+  return {
+    text: 'text222',
+    animation: 'animate-bounce',
+  } [props.loadingStyle]
+})
+
+</script>
