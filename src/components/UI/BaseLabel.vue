ebdd3a6cf262bac9d4a5726983844f660c96aa4d
Stanislav <stanislav.nechaev@gmail.com>
2023-11-14 01:45:13 +0200

    Create BaseLabel.vue

---
+++ src/components/UI/BaseLabel.vue
@@ -0,0 +1,12 @@
+<template>
+  <label
+    class="absolute left-5 top-0 origin-[0_0] truncate p-[0.37rem] leading-[1] text-neutral-600 transition-all duration-200 ease-out peer-focus:-translate-y-[0.8rem] peer-focus:scale-[0.8] peer-focus:text-primaryColor bg-white">
+    {{ label }}
+  </label>
+</template>
+<script setup>
+
+defineProps({
+  label: String,
+})
+</script>
