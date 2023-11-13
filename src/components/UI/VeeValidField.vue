672a708635ba120bc871fb19520ea0f0373a09c6
Stanislav <stanislav.nechaev@gmail.com>
2023-11-14 01:46:28 +0200

    add baseLabel

--- src/components/UI/VeeValidField.vue
+++ src/components/UI/VeeValidField.vue
@@ -6,16 +6,14 @@
            @change="$emit('change', $event.target.value)"
            class="border border-cyan-500 p-4 w-full rounded-lg peer"
     />
-    <label v-if="label"
-           class="absolute left-5 top-0 origin-[0_0] truncate p-[0.37rem] leading-[1] text-neutral-600 transition-all duration-200 ease-out peer-focus:-translate-y-[0.8rem] peer-focus:scale-[0.8] peer-focus:text-primaryColor bg-white">
-      {{ label }}
-    </label>
+    <base-label  label="Write task" v-if="label" />
     <ErrorMessage class="text-red-600" :name="name"/>
   </div>
 </template>
 
 <script setup>
 import {Field, ErrorMessage} from 'vee-validate';
+import BaseLabel from "@/components/ui/BaseLabel.vue";
 
 defineProps({
   label: String,
