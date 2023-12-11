d36dc69eba955bd812542e4d22fe15912559bda2
Stanislav <stanislav.nechaev@gmail.com>
2023-12-11 23:37:53 +0200

    Create updateStore.js

---
+++ src/stores/updateStore.js
@@ -0,0 +1,21 @@
+import {defineStore} from "pinia"
+import {ref} from "vue";
+import {getData} from "@/composables/useHandleLoadingAndError";
+import apiRouter from "@/api/apiRouter.js";
+
+const useUpdateStore = defineStore('appUpdate', () => {
+  const course = ref({});
+
+  const update = async (key, value)=>{
+    const res = await apiRouter.admin.thumbnail.create(key, value)
+    course.value = getData(res)
+    return res
+  }
+
+  return {
+    course,
+    update,
+  }
+})
+
+export default useUpdateStore;
