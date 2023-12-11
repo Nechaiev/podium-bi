7165ac5a314a525b5e111c6b8fa8d11bf127dc6c
Stanislav <stanislav.nechaev@gmail.com>
2023-12-11 23:37:20 +0200

    Create TextFieldValidate.vue

---
+++ src/components/ui/TextFieldValidate.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-text-field :label="label"
+                :placeholder="placeholder"
+                :variant="variant"
+                :type="type"
+                v-model="innerValue"
+                :error="!!hasError"
+                :error-messages="errorText"
+                class="w-full"
+                v-bind="$attrs"
+  ></v-text-field>
+</template>
+
+<script setup>
+import {useField} from "vee-validate";
+import {useGetFieldError} from "@/composables/helpers";
+
+const props = defineProps({
+  name: {
+    type: String,
+    default: "",
+  },
+  type: {
+    type: String,
+    default: "text",
+  },
+  label: String,
+  placeholder: String,
+  modelValue: String,
+  validationSchema: null,
+  syncVModel: Boolean,
+  variant: {
+    type: String,
+    default: "outlined"
+  }
+});
+defineEmits(["update:modelValue"]);
+
+const {value: innerValue, errors} = useField(
+  () => props.name,
+  props.validationSchema,
+  {
+    syncVModel: props.syncVModel,
+  },
+);
+
+const {hasError, errorText} = useGetFieldError(errors)
+
+</script>
