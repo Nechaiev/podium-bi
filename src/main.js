4da8219dbdaf2bc3a9fb92887761e6ae44d215c7
Stanislav <stanislav.nechaev@gmail.com>
2023-12-11 23:37:35 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
 import './assets/main.css'
-import {createPinia} from 'pinia'
+import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import vuetify from './plugins/vuetify'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 const app = createApp(App)
 
-const pinia = createPinia();
-// console.log(pinia.state.value.user);
+const pinia = createPinia()
 
 pinia.use(piniaPluginPersistedstate)
 app.use(router)
 app.use(pinia)
+app.use(vuetify)
 app.mount('#app')
