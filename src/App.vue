468b68329f348cff8e443ac0c036fbd881941b91
Stanislav <stanislav.nechaev@gmail.com>
2023-12-11 23:37:04 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,25 @@
   <div v-if="loading">
     loading
   </div>
-  <RouterView v-else v-slot="{ Component, route }">
-    <component :is="route.meta.layout ? route.meta.layout : AppLayout">
+  <Suspense v-else>
+    <RouterView v-slot="{ Component, route }">
+      <component :is="route.meta.layout ? route.meta.layout : AppLayout">
         <pre>{{route.meta.layout}}</pre>
         <component v-if="Component" :is="Component"></component>
       </component>
-  </RouterView>
+    </RouterView>
+  </Suspense>
 </template>
 
 
 <script setup>
-    import AppLayout from './layouts/AppLayout.vue';
-    import useAuthStore from "@/stores/authStore";
-    import useHandleLoadingAndError from "@/composables/useHandleLoadingAndError";
+import AppLayout from './layouts/AppLayout.vue';
+import useAuthStore from "@/stores/authStore";
+import useHandleLoadingAndError from "@/composables/useHandleLoadingAndError";
 
-    const authStore = useAuthStore()
-    const {handler, loading, error} = useHandleLoadingAndError({loading: true})
-    handler(authStore.fetchUser)
+const authStore = useAuthStore()
+const {handler, loading, error} = useHandleLoadingAndError({loading: true})
+handler(authStore.fetchUser)
 
 </script>
 <!--suslovros@gmail.com-->
